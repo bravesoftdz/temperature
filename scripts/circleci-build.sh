b3313a1f1168dd484b8f71264a439cda86d46f15
markfirmware <markfirmware@users.noreply.github.com>
2017-01-14 22:25:10 -0500

    Update circleci-build.sh

--- scripts/circleci-build.sh
+++ scripts/circleci-build.sh
@@ -3,9 +3,9 @@ set -x
 rm -rf artifacts/ *.bin *.elf *.img *.o *.ppu
 mkdir -p artifacts/kernels artifacts/build-test-output
 fpc -dTARGET_QEMUARM7A -B -Tultibo -O2 -Parm -CpARMV7A -WpQEMUVPB @/root/ultibo/core/fpc/bin/qemuvpb.cfg \
- src/temperature.lpr && \
+ src/ptemperature.lpr && \
 cp -a kernel.bin artifacts/kernels && \
 fpc -dTARGET_RPI3 -B -Tultibo -O2 -Parm -CpARMV7A -WpRPI3B @/root/ultibo/core/fpc/bin/rpi3.cfg \
- src/temperature.lpr && \
+ src/ptemperature.lpr && \
 cp -a kernel7.img artifacts/kernels && \
 cp -a scripts/run-qemu artifacts/kernels
