1ae6e29e8a88fcec997472653bfb25e0e843be42
markfirmware <markfirmware@users.noreply.github.com>
2017-01-14 20:26:28 -0500

    run-qemu

--- docker-build.sh
+++ docker-build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -x
 rm -rf artifacts/ *.bin *.elf *.img *.o *.ppu
-mkdir -p artifacts/kernels
+mkdir -p artifacts/kernels artifacts/build-test-output
 fpc -dTARGET_QEMUARM7A -B -Tultibo -O2 -Parm -CpARMV7A -WpQEMUVPB @/root/ultibo/core/fpc/bin/qemuvpb.cfg \
  ptemperature.lpr && \
 mv kernel.bin artifacts/kernels && \
 fpc -dTARGET_RPI3 -B -Tultibo -O2 -Parm -CpARMV7A -WpRPI3B @/root/ultibo/core/fpc/bin/rpi3.cfg \
  ptemperature.lpr && \
-mv kernel7.img artifacts/kernels
+mv kernel7.img artifacts/kernels && \
+cp -a run-qemu artifacts/kernels
