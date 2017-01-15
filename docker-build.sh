4388884e374f614ace548a6268a634e59634a48c
markfirmware <markfirmware@users.noreply.github.com>
2017-01-14 19:50:12 -0500

    Update docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -4,4 +4,4 @@ rm -f i*.bin *.elf *.img *.o *.ppu
 fpc -dTARGET_QEMUARM7A -B -Tultibo -O2 -Parm -CpARMV7A -WpQEMUVPB @/root/ultibo/core/fpc/bin/qemuvpb.cfg \
  ptemperature.lpr && \
 fpc -dTARGET_RPI3 -B -Tultibo -O2 -Parm -CpARMV7A -WpRPI3B @/root/ultibo/core/fpc/bin/rpi3.cfg \
- ptemperature.lpr2
+ ptemperature.lpr
