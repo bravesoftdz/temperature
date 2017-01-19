40e9def854124808f21f6d38a1030a70192f3faf
markfirmware <markfirmware@users.noreply.github.com>
2017-01-18 19:25:05 -0500

    build.sh

--- build.sh
+++ build.sh
@@ -31,15 +31,29 @@ function build {
      mv kernel.bin $OUTPUT
 }
 
+function intr {
+    echo $* >> $QEMU_SCRIPT
+}
+
+function qemu {
+    intr echo $*
+}
+
+SCREEN_NUMBER=1
+function screendump {
+    qemu screendump screen-$(printf "%02d" $SCREEN_NUMBER).ppm
+    ((SCREEN_NUMBER+=1))
+}
+
 function make_qemu_script {
-    touch $QEMU_SCRIPT && \
+    touch $QEMU_SCRIPT
 \
-    echo sleep 2 >> $QEMU_SCRIPT && \
-    echo echo -en \\\\001c >> $QEMU_SCRIPT && \
-    echo echo screendump screen-01.ppm >> $QEMU_SCRIPT && \
-    echo sleep 2 >> $QEMU_SCRIPT && \
-    echo echo screendump screen-02.ppm >> $QEMU_SCRIPT && \
-    echo echo quit >> $QEMU_SCRIPT && \
+    intr sleep 2
+    qemu -en \\\\001c
+    screendump
+    intr sleep 2
+    screendump
+    qemu quit
 \
     chmod u+x $QEMU_SCRIPT
 }
@@ -54,11 +68,6 @@ function run_qemu {
      -display none \
      -serial mon:stdio |& tee raw.log | egrep -iv '^(alsa|pulseaudio:|audio:)' > serial.log
 }
-function convert_screen {
-    convert $1.ppm $1.png && \
-    rm $1.ppm
-}
-
 function unix_line_endings {
     tr -d \\r < $1 > tmp && \
     mv tmp $1
@@ -71,19 +80,22 @@ build && \
 cd $OUTPUT && \
 \
 make_qemu_script && \
-run_qemu && \
-\
-convert_screen screen-01 && \
-convert_screen screen-02 && \
-\
-unix_line_endings raw.log && \
-unix_line_endings serial.log && \
+run_qemu
+
 if [ "$?" != "0" ]
 then
     exit $?
 fi
 
+unix_line_endings raw.log
+unix_line_endings serial.log
+for screen in screen*.ppm
+do
+    convert $screen ${screen%.ppm}.png
+    rm $screen
+done
 file *
+
 grep -i error serial.log
 if [ "$?" == "0" ]
 then
