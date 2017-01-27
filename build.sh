75a4d44972f822c7c23f9af268f2baec39b33d6e
markfirmware <markfirmware@users.noreply.github.com>
2017-01-27 13:28:37 -0500

    faster tcp connect and tcp read

--- build.sh
+++ build.sh
@@ -1,107 +1,245 @@
 #!/bin/bash
 
-PROGRAM=pqemuframebuffer.lpr
+# Build the ultibo projects in this repo.
+#
+# Runs on linux/x64 with either ultibo installed or docker installed.
+# Runs on windows git bash with ultibo installed.
+# Runs on windows 10 pro git bash with docker installed.
 
-OUTPUT=out
-QEMU_SCRIPT=run-qemu.tmp
+function log {
+    echo $* | tee -a $LOG
+}
 
-function docker_run {
-    which $1
-    if [ $? == 0 ]
+function ultibo-bash-quotation {
+    if [ "$(which $1)" != "" ]
     then
-        $*
+        echo -n $*
     else
-        DOCKER_IMAGE=markfirmware/ultibo-bash-v3
-        echo $1 not found ... using docker image $DOCKER_IMAGE
-        docker run --rm -v $(pwd):/workdir --entrypoint /bin/bash $DOCKER_IMAGE -c "$*"
+        local DOCKER_IMAGE=markfirmware/ultibo-bash
+        echo -en "docker run --rm -i -v $(pwd):/workdir --entrypoint /bin/bash $DOCKER_IMAGE -c \"$*\""
     fi
 }
 
-function build {
-    rm -rf obj && \
-    mkdir -p obj && \
-    docker_run fpc \
-     -B \
-     -FEobj \
-     -Tultibo \
-     -O2 \
-     -Parm \
-     -CpARMV7A \
-     -WpQEMUVPB \
-     @/root/ultibo/core/fpc/bin/qemuvpb.cfg \
-     $PROGRAM && \
-     mv kernel.bin $OUTPUT
+function ultibo-bash {
+    eval $(ultibo-bash-quotation $*)
 }
 
-function intr {
-    echo $* >> $QEMU_SCRIPT
-}
+#function intr {
+#    echo $* >> $QEMU_SCRIPT
+#}
 
-function qemu {
-    intr echo $*
-}
+#function qemu {
+#    intr echo $*
+#}
 
-SCREEN_NUMBER=1
-function screendump {
-    qemu screendump screen-$(printf "%02d" $SCREEN_NUMBER).ppm
-    ((SCREEN_NUMBER+=1))
-}
+#function screendump {
+#    qemu screendump screen-$(printf "%02d" $SCREEN_NUMBER).ppm
+#    ((SCREEN_NUMBER+=1))
+#}
 
-function make_qemu_script {
-    touch $QEMU_SCRIPT
-\
-    intr sleep 2
-    qemu -en \\\\001c
-    screendump
-    intr sleep 2
-    screendump
-    qemu quit
-\
-    chmod u+x $QEMU_SCRIPT
-}
+#function make-qemu-script {
+#    touch $QEMU_SCRIPT
+#\
+#    intr sleep 2
+#    qemu -en \\\\001c
+#    screendump
+#    intr sleep 2
+#    screendump
+#    qemu quit
+#\
+#    chmod u+x $QEMU_SCRIPT
+#}
 
-function run_qemu {
-    echo running qemu ...
-    docker_run ./$QEMU_SCRIPT | qemu-system-arm \
-     -M versatilepb \
-     -cpu cortex-a8 \
-     -kernel kernel.bin \
-     -m 256M \
-     -display none \
-     -serial mon:stdio |& tee raw.log | egrep -iv '^(alsa|pulseaudio:|audio:)' > serial.log
-}
+#function run-qemu {
+#    echo .... running qemu
+#    QEMU=$(ultibo-bash-quotation qemu-system-arm \
+#     -M versatilepb \
+#     -cpu cortex-a8 \
+#     -kernel kernel.bin \
+#     -m 256M \
+#     -display none \
+#     -serial mon:stdio)
+#    eval "./$QEMU_SCRIPT | $QEMU > raw.log 2>&1" && \
+#    cat raw.log | egrep -iv '^(alsa|pulseaudio:|audio:)' > serial.log
+#}
+
 function unix_line_endings {
     tr -d \\r < $1 > tmp && \
     mv tmp $1
 }
 
-rm -rf $OUTPUT && \
-mkdir -p $OUTPUT && \
-build && \
+function test-qemu-target {
+    echo .... running qemu
+    local RESTORE_PWD=$(pwd)
+    local FOLDER=$1
+    cd $FOLDER/$OUTPUT && \
+    \
+#   make-qemu-script && \
+#   run-qemu
+    time python $RESTORE_PWD/run-qemu
+    if [[ $? -ne 0 ]]; then log fail: $?; fi
+
+    unix_line_endings run-qemu-output/monitor.txt
+    unix_line_endings run-qemu-output/serial0.txt
+    unix_line_endings run-qemu-output/serial1.txt
+    unix_line_endings run-qemu-output/serial2.txt
+    unix_line_endings run-qemu-output/serial3.txt
+    sed -i 's/.\x1b.*\x1b\[D//' run-qemu-output/monitor.txt
+    sed -i 's/\x1b\[K//' run-qemu-output/monitor.txt
+    ls screen*.ppm > /dev/null 2>&1
+    if [[ $? -eq 0 ]]
+    then
+        for screen in screen*.ppm
+        do
+            ultibo-bash convert $screen ${screen%.ppm}.png && \
+            rm $screen
+        done
+    fi
+
+    file run-qemu-output/*
+
+#   grep -i error run-qemu-output/applog.txt	
+#   local EXIT_STATUS=$?
+
+    cd $RESTORE_PWD
+#   if [[ EXIT_STATUS == 0 ]]; then log fail: $?; fi
+}
+
+function build-example {
+    TARGETS_PATH=$1
+    if [[ -d $TARGETS_PATH ]]
+    then
+        for TARGET_PATH in $TARGETS_PATH/*
+        do
+            build-as $(basename $TARGET_PATH) $TARGET_PATH ultibohub/Examples
+        done
+    fi
+}
+
+function build-frame-buffer-qemu-memsave {
+    build-as QEMU src markfirmware/temperature/frame-buffer-qemu-memsave
+}
+
+function build-asphyre {
+    local SAMPLES_PATH=gh/ultibohub/Asphyre/Samples/FreePascal/Ultibo
+    for SAMPLE_PATH in $SAMPLES_PATH/*
+    do
+        build-as RPi2 $SAMPLE_PATH ultibohub/Asphyre
+    done
+}
+
+function build-demo {
+    DEMO_PATH=gh/ultibohub/Demo
+    for TARGET in RPi RPi2 RPi3
+    do
+        mkdir -p $DEMO_PATH/$TARGET
+        build-as $TARGET $DEMO_PATH/$TARGET ultibohub/Demo "$DEMO_PATH/UltiboDemo$TARGET.lpr"
+    done
+}
+
+function build-examples {
+    local EXAMPLES_PATH=gh/ultibohub/Examples
+    for EXAMPLE in $EXAMPLES_PATH/[0-9][0-9]-*
+    do
+        build-example $EXAMPLE
+    done
+    for EXAMPLE in $EXAMPLES_PATH/Advanced/*
+    do
+        build-example $EXAMPLE
+    done
+}
+
+function build-lpr {
+    local LPR_FILE=$1
+    local TARGET_COMPILER_OPTIONS=$2
+    local CFG_NAME=$3
+    local LPR_FOLDER=$4
+    local INCLUDES=-Fi/root/ultibo/core/fpc/source/packages/fv/src
+    log .... building $LPR_FILE
+    rm -rf $LPR_FOLDER/obj && \
+    mkdir -p $LPR_FOLDER/obj && \
+    ultibo-bash fpc \
+     -l- \
+     -v0ewn \
+     -B \
+     -Tultibo \
+     -O2 \
+     -Parm \
+     -Mdelphi \
+     -FuSource \
+     -Fugh/ultibohub/Asphyre/Source \
+     -FE$LPR_FOLDER/obj \
+     $INCLUDES \
+     $TARGET_COMPILER_OPTIONS \
+     @/root/ultibo/core/fpc/bin/$CFG_NAME \
+     $LPR_FILE |& tee -a $LOG && \
 \
-cd $OUTPUT && \
-\
-make_qemu_script && \
-run_qemu
+    mv kernel* $LPR_FOLDER/$OUTPUT
+    if [[ $? -ne 0 ]]; then log fail: $?; fi
+}
 
-if [ "$?" != "0" ]
-then
-    exit $?
-fi
+function build-as {
+    local TARGET=$1
+    local FOLDER=$2
+    local REPO=$3
+    local LPR_FILE=$4
+    if [[ -d $FOLDER ]]
+    then
+        if [[ $LPR_FILE == "" ]]
+        then
+            ls $FOLDER/*.lpr > /dev/null 2>&1
+            if [[ $? -eq 0 ]]
+            then
+                local LPR_FILE=$FOLDER/*.lpr
+            fi
+        fi
+        if [[ $LPR_FILE != "" ]]
+        then
+            rm -rf $FOLDER/$OUTPUT
+            mkdir -p $FOLDER/$OUTPUT
+            case $TARGET in
+                QEMU)
+                    build-lpr $LPR_FILE "-CpARMV7A -WpQEMUVPB" qemuvpb.cfg $FOLDER
+                    test-qemu-target $FOLDER ;;
+                RPi)
+                    build-lpr $LPR_FILE "-CpARMV6 -WpRPIB" rpi.cfg $FOLDER ;;
+                RPi2)
+                    build-lpr $LPR_FILE "-CpARMV7A -WpRPI2B" rpi2.cfg $FOLDER ;;
+                RPi3)
+                    build-lpr $LPR_FILE "-CpARMV7A -WpRPI3B" rpi3.cfg $FOLDER ;;
+            esac
+            local THISOUT=$OUTPUT/kernels-and-tests/$FOLDER
+            rm -rf $THISOUT && \
+            mkdir -p $THISOUT && \
+            cp -a $FOLDER/$OUTPUT/* $THISOUT && \
+            if [[ $? -ne 0 ]]; then log fail: $?; fi
+        fi
+    fi
+}
 
-unix_line_endings raw.log
-unix_line_endings serial.log
-sed -i 's/.\x1b.*\x1b\[D//' serial.log
-sed -i 's/\x1b\[K//' serial.log
-for screen in screen*.ppm
-do
-    docker_run convert $screen ${screen%.ppm}.png
-    rm $screen
-done
-file *
+function create-build-summary {
+    cat $LOG | egrep -i '(fail|error|warning|note):' | sort | uniq > $ERRORS
+    log
+    log Summary:
+    log
+    cat $ERRORS | tee -a $LOG
+    log
+    log $(wc $ERRORS)
+    if [[ -s $ERRORS ]]
+    then
+        exit 1
+    fi
+}
 
-grep -i error serial.log
-if [ "$?" == "0" ]
-then
-    exit 1
-fi
+OUTPUT=build-output
+QEMU_SCRIPT=run-qemu.tmp
+SCREEN_NUMBER=1
+ERRORS=build-errors.txt
+LOG=$OUTPUT/build.log
+rm -rf $OUTPUT
+mkdir -p $OUTPUT
+rm -f $LOG
+
+build-frame-buffer-qemu-memsave
+
+create-build-summary
--- src/pqemuframebuffer.lpr
+++ src/pqemuframebuffer.lpr
@@ -1,8 +1,12 @@
 program pQemuFrameBuffer;
 uses QEMUVersatilePB,Console,GlobalConfig,GlobalConst,FrameBuffer,Logging,Platform,Serial,SysUtils,Crt;
+const
+ ScreenWidth=1920;
+ ScreenHeight=1080;
 var
  Fb:pFrameBufferDevice;
  FramebufferProperties:TFramebufferProperties;
+ ScreenBuffer:Pointer;
 
 procedure StartLogging;
 begin
@@ -11,14 +15,27 @@ begin
  LoggingDeviceSetDefault(LoggingDeviceFindByType(LOGGING_TYPE_SERIAL));
 end;
 
+procedure CaptureScreen;
+const
+ ColorFormat=COLOR_FORMAT_RGB15;
+ BytesPerPixel=2;
+begin
+ if not Assigned(ScreenBuffer) then
+  ScreenBuffer:=GetMem(ScreenWidth * ScreenHeight * BytesPerPixel);
+ ConsoleDeviceGetImage(ConsoleDeviceGetDefault,0,0,ScreenBuffer,
+                       ScreenWidth,ScreenHeight,ColorFormat,0);
+ LoggingOutput(Format('frame buffer at 0x%x -size %dx%d -depth %d',
+                      [LongWord(ScreenBuffer),ScreenWidth,ScreenHeight,BytesPerPixel * 8]));
+end;
+
 procedure Test;
 begin
  Fb:=FrameBufferDeviceGetDefault;
  FrameBufferDeviceRelease(Fb);
  Sleep(100);
  FrameBufferProperties.Depth:=32;
- FrameBufferProperties.PhysicalWidth:=1024;
- FrameBufferProperties.PhysicalHeight:=768;
+ FrameBufferProperties.PhysicalWidth:=ScreenWidth;
+ FrameBufferProperties.PhysicalHeight:=ScreenHeight;
  FrameBufferProperties.VirtualWidth:=FrameBufferProperties.PhysicalWidth;
  FrameBufferProperties.VirtualHeight:=FrameBufferProperties.PhysicalHeight * 2;
  FrameBufferDeviceAllocate(Fb,@FrameBufferProperties);
@@ -26,16 +43,17 @@ begin
  FrameBufferDeviceSetDefault(Fb);
  FrameBufferDeviceGetProperties(Fb,@FrameBufferProperties);
  ConsoleWindowCreate(ConsoleDeviceGetDefault,CONSOLE_POSITION_FULLSCREEN,True);
- LoggingOutput(Format('FrameBuffer address %8.8x',[FrameBufferProperties.Address]));
  WriteLn('Test');
 end;
 
 begin
 StartLogging;
+ScreenBuffer:=nil;
 Test;
 while True do
  begin
-  LoggingOutput('Tick');
-  Sleep(1*1000);
+  WriteLn('Frame');
+  CaptureScreen;
+  Sleep(5*1000);
  end;
 end.
